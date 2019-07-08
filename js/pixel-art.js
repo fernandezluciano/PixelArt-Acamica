ca31cd4ec5a72cc7b12eefe8e267135e52baefad
Luciano Fernandez <lucianofernandez@Lucianos-MacBook-Pro.local>
2019-07-08 11:33:17 -0300

    Guia 2/3 terminada.

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -24,8 +24,8 @@ var paleta = document.getElementById("paleta");
 
 var grillaPixeles = document.getElementById("grilla-pixeles");
 
-var crearPaletaColores = function (nombreColores) {
-  for (var i = 0; i < nombreColores.length; i++) {
+var crearPaletaColores = function(nombreColores){
+  for (var i = 0; i < nombreColores.length; i++){
    var color = document.createElement("div");
    color.style.backgroundColor = nombreColores[i];
    color.className = "color-paleta";
@@ -34,30 +34,56 @@ var crearPaletaColores = function (nombreColores) {
 }
 crearPaletaColores(nombreColores);
 
-var crearGrilla = function () {
+var crearGrilla = function(){
   var i = 0;
-  while (i < 1750) {
+  while (i < 1750){
     var nuevoPixel = document.createElement("div");
+    nuevoPixel.setAttribute("class", "pixel");
     grillaPixeles.appendChild(nuevoPixel);
     i++
   }
 }
 crearGrilla();
 
+$("div.color-paleta").click(function(e){
+  var colorActual = e.target.style.backgroundColor;
+  $("#indicador-de-color").css({"backgroundColor": colorActual});
+});
 
-
-
+$(".pixel").click(function(){
+  var colorSeleccionado = document.getElementById("indicador-de-color");
+  var colorActual = colorSeleccionado.style.backgroundColor;
+  $(this).css({"backgroundColor": colorActual});
+});
 
 // Variable para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
-var colorPersonalizado = document.getElementById('color-personalizado');
+var colorPersonalizado = document.getElementById("color-personalizado");
 
-colorPersonalizado.addEventListener('change', 
-  (function() {
+colorPersonalizado.addEventListener("change", (function(){
     // Se guarda el color de la rueda en colorActual
     colorActual = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
-
-
+    $("#indicador-de-color").css({"backgroundColor": colorActual});
   })
 );
+
+var mouseApretado = false;
+
+var grillaPixeles = document.getElementById("grilla-pixeles");
+
+$("#grilla-pixeles").mousedown(function(){
+  mouseApretado = true;
+});
+
+$("#grilla-pixeles").mouseup(function(){
+  mouseApretado = false;
+});
+
+grillaPixeles.addEventListener("mouseover", function(e){
+  if(mouseApretado === true){
+    colorSeleccionado = document.getElementById("indicador-de-color");
+    colorActual = colorSeleccionado.style.backgroundColor;
+    e.target.style.backgroundColor = colorActual;
+  }
+});
